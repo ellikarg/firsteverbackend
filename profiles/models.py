9dd9c9fe7135d24fdb83808ac6e14eb4c84a5640
elli karg <elisa.karg.bogor@gmail.com>
2023-10-23 11:18:00 +0000

    feat: add posts app with model, views, serializers

--- profiles/models.py
+++ profiles/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    """
+    Profile model with default image
+    """
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
