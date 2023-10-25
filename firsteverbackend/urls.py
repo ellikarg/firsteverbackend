92d90ff75b3c536016e4496ad53ad600e7d6c453
elli karg <elisa.karg.bogor@gmail.com>
2023-10-25 12:10:32 +0000

    deploy: add home view

--- firsteverbackend/urls.py
+++ firsteverbackend/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import root_route
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +29,5 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
+    path('', root_route),
 ]
