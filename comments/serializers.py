331e341998e88857b5817936569da7605f73af4c
elli karg <elisa.karg.bogor@gmail.com>
2023-10-25 12:17:50 +0000

    deploy: add date and time formatting

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Comment
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -11,6 +12,8 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -22,6 +25,12 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'created_at', 'updated_at', 'content'
         ]
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+        
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
 
 class CommentDetailSerializer(CommentSerializer):
