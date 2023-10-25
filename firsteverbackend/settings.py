85431d1097f2dfa3b0e785e5a9d3d3ce1847e2be
elli karg <elisa.karg.bogor@gmail.com>
2023-10-25 12:12:19 +0000

    deploy: add pagination

--- firsteverbackend/settings.py
+++ firsteverbackend/settings.py
@@ -77,7 +77,14 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ 
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )]
+    'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
     }
+
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'restframework.renderers.JSONRenderer'
+    ]
 
 REST_USE_JWT = True
 
