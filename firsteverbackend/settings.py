3a2717f327cc5e7fc892b40770855d27501ae79e
elli karg <elisa.karg.bogor@gmail.com>
2023-10-25 12:40:06 +0000

    deploy: add env variables

--- firsteverbackend/settings.py
+++ firsteverbackend/settings.py
@@ -34,10 +34,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_6(tf$%-y*zq#=8fm+oz+4ck2mbpjrpldqn#62@6yw7yss3(s4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'firsteverbackend.herokuapp.com']
 
