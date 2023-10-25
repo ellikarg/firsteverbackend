ac39f853b7aee1093422e9df1088687e49db30d2
elli karg <elisa.karg.bogor@gmail.com>
2023-10-25 13:03:56 +0000

    debug: add allowed host ['*']

--- firsteverbackend/settings.py
+++ firsteverbackend/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'firsteverbackend.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'firsteverbackend.herokuapp.com', '*']
 
 
 # Application definition
