a1357effe6212162174c34d13fb0f9ed84909e45
elli karg <elisa.karg.bogor@gmail.com>
2023-10-26 11:27:55 +0000

    debug: set debug to true

--- firsteverbackend/settings.py
+++ firsteverbackend/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'firsteverbackend.herokuapp.com',
