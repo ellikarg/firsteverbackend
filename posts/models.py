515eac44f1f9550fe0f1f97e65d10be4429c1cc0
elli karg <elisa.karg.bogor@gmail.com>
2023-11-02 18:53:30 +0000

    feat: add description field to Post model

--- posts/models.py
+++ posts/models.py
@@ -9,7 +9,8 @@ class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=80)
+    description = models.TextField(max_length=160, null=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_post_i2ll5s', blank=True
