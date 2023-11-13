723f3c94638779f407067a7f23c5fd365ecaf800
elli karg <elisa.karg.bogor@gmail.com>
2023-11-13 17:40:46 +0000

    feat: add created_at & updated_at to category model

--- categories/models.py
+++ categories/models.py
@@ -16,9 +16,12 @@ class Category(models.Model):
         Post,
         related_name='categories',
         blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = 'categories'
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.name
