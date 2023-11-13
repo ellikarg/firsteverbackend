f0d41cf6759af11b84248fd055fdf799468674df
elli karg <elisa.karg.bogor@gmail.com>
2023-11-13 08:25:48 +0000

    feat: add list and details view for categories

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Category
+from rest_framework import generics, permissions
+from firsteverbackend.permissions import IsOwnerOrReadOnly
+from .serializers import CategorySerializer
 
-# Create your views here.
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsOwnerOrReadOnly]
