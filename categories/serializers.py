a19f2ea8345379c1368ba7c95b29c5c58341dbf6
elli karg <elisa.karg.bogor@gmail.com>
2023-11-14 21:22:07 +0000

    feat: remove post from category serializer

--- categories/serializers.py
+++ categories/serializers.py
@@ -3,8 +3,7 @@ from .models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    post = serializers.ReadOnlyField(source='post.id')
 
     class Meta:
         model = Category
-        fields = ['id', 'name', 'owner', 'post']
+        fields = ['id', 'name', 'owner']
