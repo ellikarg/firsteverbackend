feb6120ffa6fdaa4f369d42c3359e43396e8c311
elli karg <elisa.karg.bogor@gmail.com>
2023-11-15 10:37:11 +0000

    feat: add category_name to PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -11,6 +11,7 @@ class PostSerializer(serializers.ModelSerializer):
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
+    category_name = serializers.ReadOnlyField(source='category.name')
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -44,5 +45,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'description', 'content', 'image', 'like_id',
-            'likes_count', 'comments_count', 'category',
+            'likes_count', 'comments_count', 'category', 'category_name'
         ]
