3ef29163747cca7fb69f2228e27babad09f6fcc4
elli karg <elisa.karg.bogor@gmail.com>
2023-11-15 16:23:31 +0000

    debug: delete 2nd migration of profile model

--- profiles/migrations/0003_alter_profile_image.py
+++ profiles/migrations/0003_alter_profile_image.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('profiles', '0002_alter_profile_image'),
+        ('profiles', '0001_initial'),
     ]
 
     operations = [
