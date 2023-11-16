3ed509435b73ba426e25e285f614d3612eaf1529
elli karg <elisa.karg.bogor@gmail.com>
2023-11-16 17:28:43 +0000

    feat: alter default profile image

--- profiles/models.py
+++ profiles/models.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_vexrlp'
+        upload_to='images/', default='../default_profile_bst2p6'
     )
 
     class Meta:
